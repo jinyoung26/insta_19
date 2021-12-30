fbe8f94c093629889d59ed274e8a50624c7908ef
jinyoung26 <01051754107a@gmail.com>
2021-12-30 15:40:04 +0900

    redirect mypage

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template # 플라스크
+from flask import Flask, render_template, redirect # 플라스크
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('main.html')
 
+@app.route('/mypage')
+def mypage():
+   return render_template('mypage.html')
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
-
-   # 진영님 여기 바뀌었어요 ! 주석 추가
