eedd9a2377b772638cde7b4d0e85b61dc70eefa1
jinyoung26 <01051754107a@gmail.com>
2021-12-30 13:49:41 +0900

    19-2

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # 플라스크
 app = Flask(__name__)
 
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:')
-db =
-
 @app.route('/')
-def main():
-   return render_template("main.html")
+def home():
+   return render_template('main.html')
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
